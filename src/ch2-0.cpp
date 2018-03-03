4575b91efb13061d6d4bb6e65b7552f8f11cd6c9
william merfalen <gamedev@bnull.net>
2018-03-03 11:40:25 +0000

    simplified my code a bit

--- src/ch2-0.cpp
+++ src/ch2-0.cpp
@@ -8,33 +8,23 @@
 #include <iostream>
 #include <type_traits>
 
-template <typename T,bool is_const_ref>
-struct add_const_ref_impl { };
-
-template <typename T>
-struct add_const_ref_impl <T,true> {
-	typedef T type;
-};
-template <typename T>
-struct add_const_ref_impl <T,false> {
-	typedef T const & type;
-};
 template <typename T>
 struct add_const_ref {
-	typedef typename add_const_ref_impl<T,std::is_reference<T>::value>::type type;
+	using type = T const &;
+};
+template <typename T>
+struct add_const_ref<T&> {
+	using type = T;
 };
 
 /* Our test data type */
 struct A {};
 int main(int argc,char** argv){
-	constexpr bool is_same = std::is_same<typename add_const_ref<A>::type,A const &>::value;
-	std::cout << "[test-case 0] add_const_ref<A> +  A const & ";
+	constexpr bool is_same = std::is_same<typename add_const_ref<A&>::type,A>::value;
+	std::cout << "[test-case 0] add_const_ref<A&> +  A ";
 	std::cout << (is_same ? "[ same ]" : "[ different ]") << "\n";
-	constexpr bool is_same2 = std::is_same<typename add_const_ref<A const &>::type,A const &>::value;
-	std::cout << "[test-case 1] add_const_ref<A const &> +  A const & ";
-	std::cout << (is_same2 ? "[ same ]" : "[ different ]") << "\n";
-	constexpr bool is_same3 = std::is_same<typename add_const_ref<A>::type,A&>::value;
-	std::cout << "[test-case 2] add_const_ref<A> +  A & ";
-	std::cout << (is_same3 ? "[ same ]" : "[ different ]") << "\n";
+	constexpr bool is_same1 = std::is_same<typename add_const_ref<A>::type,A const &>::value;
+	std::cout << "[test-case 1] add_const_ref<A> +  A const & ";
+	std::cout << (is_same1 ? "[ same ]" : "[ different ]") << "\n";
 	return 0;
 }
